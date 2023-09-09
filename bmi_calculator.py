e52ea1b72d7aced50b68a91d0f51f82491969167
kirthi-suresh <kirthi.suresh@outlook.com>
2023-09-09 06:01:23 -0400

    code update

--- bmi_calculator.py
+++ bmi_calculator.py
@@ -1,12 +1,13 @@
 """
 Calculate Body Mass Index
 
-Version 1.0.0
+Version 1.0.1
 """
+from common_utils import print_header
+
 
 program = f"Calculate Body Mass Index"
-print(program)
-print("+" * len(program))
+print_header(program)
 
 height = float(input("What is the height in m?\n"))
 weight = float(input("What is the weight in kg?\n"))
@@ -14,3 +15,14 @@ weight = float(input("What is the weight in kg?\n"))
 bmi = weight / (height**2)
 
 print(f"BMI is {bmi:.1f}")
+
+if bmi < 18.5:
+    print("Underweight")
+elif bmi < 25:
+    print("Normal weight")
+elif bmi < 30:
+    print("Overweight")
+elif bmi < 35:
+    print("Obese")
+else:
+    print("Clinically obese")
