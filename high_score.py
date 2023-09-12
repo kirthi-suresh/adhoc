213c388f798c45db538acb476bc135d6bf758d11
Kirthi Suresh <kirthi.suresh@outlook.com>
2023-09-12 14:24:40 -0400

    code update

--- high_score.py
+++ high_score.py
@@ -11,25 +11,18 @@ from common_utils import print_header
 header = f"High Score Display"
 print_header(header)
 
-student = ['Lily','Jason','Mike','Sarah']
-score = [94,76,83,80]
+students = ['Lily','Jason','Mike','Sarah']
+scores = [94,76,83,80]
 
-subject = zip(student,score)
+for i,student in enumerate(students):
+    print(f"{student:6} - {scores[i]}")
 
-highest_score = 0
-lowest_score = 100
-top_student = ''
-last_student = ''
 
-print("Marks:")
-for student, score in subject:
-    if score > highest_score:
-        highest_score = score
-        top_student = student
-    if score < lowest_score:
-        lowest_score = score
-        last_student = student
-    print(f"{student:10} - {score}")
+highest_score = max(scores)
+top_student = students[scores.index(highest_score)]
+
+lowest_score = min(scores)
+last_student = students[scores.index(lowest_score)]
 
 print(f"{top_student} scores the highest with {highest_score} marks")
 print(f"{last_student} scores the last with {lowest_score} marks")
