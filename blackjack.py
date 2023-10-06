c24c337219dc1c2d3794e37d25607f9707056eb6
kirthi-suresh <kirthi.suresh@outlook.com>
2023-10-06 05:28:41 -0400

    Blackjack

---
+++ blackjack.py
@@ -0,0 +1,130 @@
+"""
+Blackjack
+
+Version 1.0.0
+"""
+import os
+import time
+import random
+import common_utils
+
+cards = {}
+
+
+def print_header():
+    os.system("cls")
+    header = f"Blackjack"
+    common_utils.print_header(header)
+
+
+def decks(n):
+    rank = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+    suit = ["Spade", "Heart", "Diamond", "Club"]
+    deck = [i + " " + j for i in suit for j in rank]
+    decks = ["D" + str(i) + "-" + j for i in range(1, n + 1) for j in deck]
+    return decks
+
+
+def value_of_card(card):
+    face = card.split(" ")[1]
+    if face == "A":
+        return 11
+    elif face in ("10", "J", "Q", "K"):
+        return 10
+    else:
+        return int(face)
+
+
+def generate_dict_cards(n):
+    for i in decks(n):
+        cards[i] = value_of_card(i)
+
+
+def count_of_decks():
+    checkpoint = True
+    while checkpoint:
+        try:
+            count_of_decks = int(input("How many decks? (1-10) "))
+            if count_of_decks > 0 and count_of_decks < 11:
+                checkpoint = False
+            else:
+                print("Invalid input!")
+                time.sleep(2)
+        except Exception as e:
+            print("Invalid input!")
+            time.sleep(2)
+    return count_of_decks
+
+
+def pick_a_card():
+    card, value = random.choice(list(cards.items()))
+    cards.pop(card)
+    return card, value
+
+
+def main():
+    print_header()
+    generate_dict_cards(count_of_decks())
+
+    dealer_cards = []
+    dealer_count = 0
+    dealer_A_flag = False
+    player_cards = []
+    player_count = 0
+    player_A_flag = False
+
+    print_header()
+    # Auto Dealer's turn
+    card, value = pick_a_card()
+    dealer_cards.append(card)
+    dealer_count += value
+    print(f"Dealer: {dealer_cards} Count: {dealer_count}")
+
+    # Auto Player's turn
+    card, value = pick_a_card()
+    player_cards.append(card)
+    player_count += value
+    print(f"Player: {player_cards} Count: {player_count}")
+
+    # Players turn
+    checkpoint = True
+    while checkpoint:
+        if player_count > 21:
+            checkpoint = False
+        elif input("Deal? (y/n)").lower() == "n":
+            checkpoint = False
+        else:
+            card, value = pick_a_card()
+            player_cards.append(card)
+            player_count += value
+            print_header()
+            print(f"Dealer: {dealer_cards} Count: {dealer_count}")
+            print(f"Player: {player_cards} Count: {player_count}")
+
+    # Dealers turn
+    checkpoint = True
+    while checkpoint:
+        if dealer_count > 16:
+            checkpoint = False
+        else:
+            card, value = pick_a_card()
+            dealer_cards.append(card)
+            dealer_count += value
+            print_header()
+            print(f"Dealer: {dealer_cards} Count: {dealer_count}")
+            print(f"Player: {player_cards} Count: {player_count}")
+
+    if player_count > 21:
+        print("Game lost!")
+    elif dealer_count > 21:
+        print("Game won!")
+    elif dealer_count == player_count:
+        print("Game draw!")
+    elif player_count > dealer_count:
+        print("Game won!")
+    else:
+        print("Game lost!")
+
+
+if __name__ == "__main__":
+    main()
